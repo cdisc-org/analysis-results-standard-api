9a8e2517aa16e0c6cc0d17ab4515bf0e337a8907
Jared Schreibman <jschreibman@cdisc.org>
2023-07-09 14:35:56 -0500

    MVP for hackathon

--- README.md
+++ README.md
@@ -1,38 +1,35 @@
 # Analysis Result Standard API (ARS)
 
-This is a template for COSA workshops/open source projects which could be used to create new projects.
+This is the api service for Analysis Result Standard
 
-## How to use this template (remove this section)
+# *ARS API* 
 
-You can copy this template and use this for your open source project, workshops, hackathons or similar where participants should contribute content. Please update the README and other files depending on your requirements, project description etc. You find various "How Tos" documents if you or your expected contributors are not familar with setting up and working with repositories. Feel free to remove no longer applicable ones for your own repository.
-
-You might want to checkout [makeareadme.com](https://www.makeareadme.com) to gain additional guidance and options for a typical README file.
-
-![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-blue.svg)
-
-**License**: This template is using [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/). So you can copy or fork this template without the need to reference. Keep attribution from CODE_OF_CONDUCT as this uses CC-BY-4.0. The license files contained are meant to be used for the final open source project you run.
-
-TODO:
-
-* update readme for your project needs, e.g. use CC0 or CC-BY-4.0 license
-* update LICENSE, [INCLUDE COPYRIGHT INSTANCE] -> e.g. Project XY, CDISC, John Doe
-* update CODE_OF_CONDUCT, [INSERT CONTACT METHOD] -> e.g. <example@example.com>
-* update CONTRIBUTION.md if you want to provide additional rules or guidances, for example naming conventions
-
-*-------------------------- DELETE EVERYTHIG ABOVE ---------------------------*
-
-
-# *NAME* 
-
-This repository should be used for the *Project/Workshop/Hackathon XY* to contain code, minutes, notes, outcomes, discussions and more. It is meant to allow anyone working with the results and discussions to allow flexible collaboration and follow up projects.
+This repository should be used for the *ARS Hackathon* to contain code, minutes, notes, outcomes, discussions and more. It is meant to allow anyone working with the results and discussions to allow flexible collaboration and follow up projects.
 
 ## Description
 
-The main purpose of *Project Name* is to ....
+The main purpose of *ARS Hackathon* is to find creative uses of ARS and it's model
 
-If possible use images as well.
+![ARS Model Image](./workfiles/ARS.jpg)
 
-![Image Description](./images/placeholder.png)
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Use uvicorn to start a WSGI server locally.
+```bash
+uvicorn main:app
+```
+
+By default, the server will start listening to port 8000 for traffic. Use `http://localhost:8000/docs` to explore the Swagger documentation of the ARS service.
+
+![Swagger Landing](./images/swagger_example.png)
 
 ## Contribution
 
--- main.py
+++ main.py
@@ -16,14 +16,14 @@ with open(phuse_data) as f:
     database.append(json.load(f))
 
 
-@ars_router.get("/packages/")
+@ars_router.get("/reportingevents/")
 def get_all_ars_packages():
     return database
     
 
-@ars_router.get("/packages/{package_id}/reportingevents/")
-def get_all_package_reporting_events(package_id):
-    return database[int(package_id)]
+# @ars_router.get("/packages/{package_id}/reportingevents/")
+# def get_all_package_reporting_events(package_id):
+#     return database[int(package_id)]
     
 
 @ars_router.get("/reportingevents/{reportingevent_id}/")
@@ -31,214 +31,214 @@ def get_reporting_event(reportingevent_id):
     return database[int(reportingevent_id)]
     
 
-@ars_router.get("/reportingevents/{reportingevent_id}/methods/")
-def get_all_reportingevents_methods(reportingevent_id):
-    return database[int(reportingevent_id)].get("methods")
+# @ars_router.get("/reportingevents/{reportingevent_id}/methods/")
+# def get_all_reportingevents_methods(reportingevent_id):
+#     return database[int(reportingevent_id)].get("methods")
     
 
-@ars_router.get("/methods/{method_id}/")
-def get_method(method_id):
-    return database[0].get("methods")[int(method_id)]
+# @ars_router.get("/methods/{method_id}/")
+# def get_method(method_id):
+#     return database[0].get("methods")[int(method_id)]
 
 
-@ars_router.get("/methods/{method_id}/operations/")
-def get_all_methods_operations(method_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/methods/{method_id}/operations/")
+# def get_all_methods_operations(method_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/operations/{operation_id}/")
-def get_operation(operation_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/operations/{operation_id}/")
+# def get_operation(operation_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/operations/{operation_id}/refoprels/")
-def get_all_operations_referenced_operation_relationship(operation_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/operations/{operation_id}/refoprels/")
+# def get_all_operations_referenced_operation_relationship(operation_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/refoprels/{refoprel_id}/")
-def get_referenced_operation_relationship(refoprel_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/refoprels/{refoprel_id}/")
+# def get_referenced_operation_relationship(refoprel_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/reportingevents/{reportingevent_id}/analysissets/")
-def get_all_reportingevents_analysissets(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/analysissets/")
+# def get_all_reportingevents_analysissets(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/analysissets/{analysisset_id}/")
-def get_analysisset(analysisset_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analysissets/{analysisset_id}/")
+# def get_analysisset(analysisset_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/reportingevents/{reportingevent_id}/analyses")
-def get_all_reportingevents_analysis(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/analyses")
+# def get_all_reportingevents_analysis(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/analyses/{analysis_id}/")
-def get_analysis(analysis_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analyses/{analysis_id}/")
+# def get_analysis(analysis_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/analyses/{analysis_id}/datagroupings/")
-def get_all_analyses_datagrouping(analysis_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analyses/{analysis_id}/datagroupings/")
+# def get_all_analyses_datagrouping(analysis_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/datagroupings/{datagrouping_id}/")
-def get__datagrouping(datagrouping_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/datagroupings/{datagrouping_id}/")
+# def get__datagrouping(datagrouping_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
     
 
-@ars_router.get("/datagroupings/{datagrouping_id}/datagroups/")
-def get_all_datagroupings_datagroup(datagrouping_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/datagroupings/{datagrouping_id}/datagroups/")
+# def get_all_datagroupings_datagroup(datagrouping_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/reportingevents/{reportingevent_id}/datasubsets/")
-def get_all_reportingevents_datasubset(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/datasubsets/")
+# def get_all_reportingevents_datasubset(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/datasubsets/{datasubset_id}/")
-def get_datasubset(datasubset_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/datasubsets/{datasubset_id}/")
+# def get_datasubset(datasubset_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/reportingevents/{reportingevent_id}/analysisgroupings/")
-def get_all_reportingevents_analysisgrouping(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/analysisgroupings/")
+# def get_all_reportingevents_analysisgrouping(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/analysisgroupings/{analysisgrouping_id}/")
-def get_analysisgrouping(analysisgrouping_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analysisgroupings/{analysisgrouping_id}/")
+# def get_analysisgrouping(analysisgrouping_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/analysisgroupings/{analysisgrouping_id}/analysisgroups/")
-def get_all_analysisgrouping_analysisgroups(analysisgroup_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analysisgroupings/{analysisgrouping_id}/analysisgroups/")
+# def get_all_analysisgrouping_analysisgroups(analysisgroup_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/analysisgroups/{analysisgroup_id}/")
-def get_analysisgroup(analysisgroup_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/analysisgroups/{analysisgroup_id}/")
+# def get_analysisgroup(analysisgroup_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/reportingevents/{reportingevent_id}/outputs/")
-def get_all_reportingevents_output(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/outputs/")
+# def get_all_reportingevents_output(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/outputs/{output_id}")
-def get_output(output_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/outputs/{output_id}")
+# def get_output(output_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/outputs/{output_id}/displays/")
-def get_all_outputs_display(output_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/outputs/{output_id}/displays/")
+# def get_all_outputs_display(output_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/displays/{display_id}/")
-def get_display(display_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/displays/{display_id}/")
+# def get_display(display_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/reportingevents/{reportingevent_id}/categorizations/")
-def get_all_reportingevents_categorization(reportingevent_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/reportingevents/{reportingevent_id}/categorizations/")
+# def get_all_reportingevents_categorization(reportingevent_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/categorizations/{categorization_id}/")
-def get_categorization(categorization_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/categorizations/{categorization_id}/")
+# def get_categorization(categorization_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/categorizations/{categorization_id}/categories/")
-def get_all_categorizations_category(categorization_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/categorizations/{categorization_id}/categories/")
+# def get_all_categorizations_category(categorization_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
-@ars_router.get("/categories/{category_id}/")
-def get_category(category_id_id):
-    data = None
-    with open(database) as f:
-        data = json.load(f)
-    return data
+# @ars_router.get("/categories/{category_id}/")
+# def get_category(category_id_id):
+#     data = None
+#     with open(database) as f:
+#         data = json.load(f)
+#     return data
 
 
 app.include_router(ars_router)
